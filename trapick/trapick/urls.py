0185126ff92896b18c8a349e072d37058997f8a4
NurAli <gt201901065@wmsu.edu.ph>
2025-11-06 20:02:29 +0800

    Update urls.py

--- trapick/trapick/urls.py
+++ trapick/trapick/urls.py
@@ -18,10 +18,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path, re_path
+from django.views.generic import TemplateView 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', include("trapickapp.urls")),  # API routes
+    path('', include("trapickapp.urls")),
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
